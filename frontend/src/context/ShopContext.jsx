98243eba96518a34e7e820079690e6baf5064de8
Marianne C <Mariannecampguilhem@gmail.com>
2024-10-29 12:43:41 +0100

    add of a navBar component for the collection page

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,26 +1,25 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/assets";
-export const ShopContext = createContext(); 
+export const ShopContext = createContext();
 
-const ShopContextProvider = (props)=> {
-const currency = '$'
-const delivery_fee = 10;
+const ShopContextProvider = (props) => {
+  const currency = "$";
+  const delivery_fee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(true);
+  const value = {
+    products,
+    currency,
+    delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
+  };
 
-const value = {
+  return (
+    <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
+  );
+};
 
-products, currency,delivery_fee
-
-}
-
-
-return (
-    <ShopContext.Provider value={value}>
-        {props.children}
-    </ShopContext.Provider>
-)
-
-
-
-}
-
-export default ShopContextProvider
+export default ShopContextProvider;
