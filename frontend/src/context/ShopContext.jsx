700717b0ea994f50b1e25ba06e91a16278eb0350
Marianne C <Mariannecampguilhem@gmail.com>
2024-10-30 13:39:07 +0100

    Develop cart by adding a price indicator, selected size display, and a functional icon to remove items from the cart

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,36 +1,64 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 export const ShopContext = createContext();
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ShopContextProvider = (props) => {
   const currency = "$";
   const delivery_fee = 10;
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(true);
-  const[CartItems,setCartItems] =useState({});
+  const [CartItems, setCartItems] = useState({});
 
-const addToCart = async (itemId,size) => {
-    let cartData = structuredClone(CartItems) ;
-
-    if(cartData[itemId]){
-        if (cartData[itemId][size]){
-            cartData[itemId][size] += 1 ;
-        }
-        else{
-            cartData[itemId][size]=1 ;
-        }
+  const addToCart = async (itemId, size) => {
+    if (!size) {
+      toast.error("select Product size Please", {
+        className: "bg-red rounded-lg border-gray-800 shadow-lg",
+      });
+      {
+        /* modifier le style plus tard avec un fichier css style  */
+      }
+      return;
     }
-else {
-    cartData[itemId] = {};
-    cartData[itemId][size] =1;
-}
-setCartItems(cartData);
-}
+    let cartData = structuredClone(CartItems);
 
-useEffect(()=>{
-    console.log(CartItems);
-},[CartItems])
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+    setCartItems(cartData);
+  };
 
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const items in CartItems) {
+      for ( const item in CartItems[items]) {
+       try {
+          if (CartItems[items][item]>0) {
+            totalCount += CartItems[items][item] ;
+          }
+        } catch (error){
+
+        }
+      }
+    }
+    return totalCount;
+  }
+
+  const updateQuantity = async (itemId,size,quantity) => {
+    let cartData = structuredClone (CartItems) ;
+
+    cartData [itemId][size]=quantity;
+
+    setCartItems(cartData);
+  }
 
   const value = {
     products,
@@ -41,7 +69,9 @@ useEffect(()=>{
     showSearch,
     setShowSearch,
     CartItems,
-    addToCart
+    addToCart,
+    getCartCount,
+    updateQuantity
   };
 
   return (
