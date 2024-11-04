c67c01a5b37b81509fe43977b9a92eb75d4db4c4
Marianne C <Mariannecampguilhem@gmail.com>
2024-11-04 16:35:42 +0100

    Implemented conditional rendering for Login/Sign Up form with responsive styling

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const[currentState , setCurrentState] = useState('Sign up')
   return (
     <div>
+      <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+        <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+    <p className='prata-regular text-3xl'> {currentState}</p>
+    <hr className='border-none h-[1.5px] w-8 bg-gray-800'/>
+     
+        </div>
+        {currentState==='Login' ? '' : <input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Name' required/> /**Quand le compte est nouveau on bine le input "name" si c'est juste un utilisateur qui veutt se log il n'apparait pas  */}  
+        <input type="email" className='w-full px-3 py-2 border border-gray-800' placeholder='Email' required />
+        <input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Password' required/>
+        <div className='w-full flex justify-between textt-sm mt-[-8px]'>
+        <p className='cursor-pointer'>Forgot your password ? </p>
+        {
+          currentState === 'Login'
+          ? <p onClick={()=> setCurrentState('Sign Up')} className='cursor-pointer'> Create account </p>
+          : <p  onClick={ ()=> setCurrentState('Login') } className='cursor-pointer' >Login here</p>
+        }
+        </div>
+      </form>
       
     </div>
   )
